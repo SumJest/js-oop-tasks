f95169f35a2c3c41ffc1135b9266efd064fd5313
SumJest <summedjesters@gmail.com>
2023-11-06 11:42:50 +0500

    do homework

--- src/es6.js
+++ src/es6.js
@@ -1,30 +1,56 @@
 "use strict";
-// в данных задачах нужно использовать возможности es6
-// ко всем заданиям можно дописать свои тесты в файле es6.spec.js
-// Можно менять параметры функций (например сделать им значения по умолчанию)
 
-// Напишите функцию, которая принимает ФИО пользователя и возвращает
-// строку формата Имя Фамилия
-function fioToName(fio) {}
+// Функция для преобразования ФИО в формат "Имя Фамилия"
+function fioToName(fio) {
+    const [lastName, firstName, middleName] = fio.split(' ');
+    return `${firstName} ${lastName}`;
+}
 
-// преобразуйте массив чисел так, чтобы в нем остались только
-// уникальные элементы
-// присмотритесь к коллекции "Set"
-function filterUnique(array) {}
+// Функция для фильтрации уникальных элементов в массиве
+function filterUnique(array) {
+    return [...new Set(array)];
+}
 
-// Задача: разница зарплат
-// в функцию приходит массив из n зарплат сотрудников фирмы
-// ваша задача определить, во сколько раз зарплата самого высокооплачиваемого
-// сотрудника превышает зарплату самого низкооплачиваемого
-// присмотритесь к методу .reduce
-function calculateSalaryDifference(array) {}
+// Функция для определения разницы в зарплатах
+function calculateSalaryDifference(array) {
+    if (array.length === 0) {
+        return 0; // Если массив пустой, возвращаем 0.
+    }
 
-// Реализуйте класс "словарь слов" (как толковый словарь)
-// класс должен быть безопасным и работать только со словами
-// присмотритесь к коллекции "Map"
-// Словарь - (string, string), и все это не null и не undefined
-// * покройте класс тестами
-class Dictionary {}
+    const maxSalary = array.reduce((max, current) => (current > max ? current : max), array[0]);
+    const minSalary = array.reduce((min, current) => (current < min ? current : min), array[0]);
+
+    if (minSalary === 0) {
+        return "Зарплата не может быть нулевой.";
+    }
+
+    return maxSalary / minSalary;
+}
+
+// Реализация класса "словарь слов" с использованием Map
+class Dictionary {
+    constructor() {
+        this.words = new Map();
+    }
+
+    addWord(key, value) {
+        if (typeof key === 'string' && typeof value === 'string') {
+            this.words.set(key, value);
+        }
+    }
+
+    getWord(key) {
+        return this.words.get(key);
+    }
+
+    deleteWord(key) {
+        this.words.delete(key);
+    }
+
+    getAllWords() {
+        return [...this.words.keys()];
+    }
+}
 
 module.exports = {
     fioToName,
--- src/oop.js
+++ src/oop.js
@@ -3,7 +3,16 @@
  * Если координаты не переданы - 0,0; Аналогично если только 1 координата.
  * Со звездочкой: реализовать метод, который возвращает расстояние от точки до центра координат (0, 0)
  */
-class Point {}
+class Point {
+    constructor(x = 0, y = 0) {
+        this.x = x;
+        this.y = y;
+    }
+
+    distanceToOrigin() {
+        return Math.sqrt(this.x ** 2 + this.y ** 2);
+    }
+}
 
 /**
  * Напишите класс геометрической точки в трехмерном пространстве (x, y, z),
@@ -11,7 +20,17 @@ class Point {}
  * Реализовать статический метод, который возвращает расстояние между Point3D.
  */
 class Point3D extends Point {
-    static vectorLength(a, b) {}
+    constructor(x = 0, y = 0, z = 0) {
+        super(x, y);
+        this.z = z;
+    }
+
+    static vectorLength(a, b) {
+        const dx = a.x - b.x;
+        const dy = a.y - b.y;
+        const dz = a.z - b.z;
+        return Math.sqrt(dx ** 2 + dy ** 2 + dz ** 2);
+    }
 }
 
 /**
@@ -20,7 +39,17 @@ class Point3D extends Point {
  * Со звездочкой: написать тесты методы класса (oop.spec.js)
  */
 class Queue {
+    constructor(items = []) {
+        this.items = items;
+    }
 
+    enqueue(item) {
+        this.items.push(item);
+    }
+
+    dequeue() {
+        return this.items.shift();
+    }
 }
 
 module.exports = {
